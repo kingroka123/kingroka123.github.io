c578cce78b9a77623a50456a25ea1998a347b337
Justin <justintheophile@gmail.com>
2019-06-24 21:49:05 -0400

    changes

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,5 +1,6 @@
+
+
 //This is the "Offline copy of pages" service worker
-console.log("hello")
 //Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener('install', function(event) {
   var indexPage = new Request('app.html');
