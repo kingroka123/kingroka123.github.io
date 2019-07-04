39324833f453efaa44f0344e9b380560b0a75a13
Justin T <hunterten1@gmail.com>
2019-07-03 22:51:35 -0400

    minor changes

--- renderer.js
+++ renderer.js
@@ -1,10 +1,10 @@
-var isElectron  = false;
+var isElectron = false;
 var cmd = require('node-cmd');
 const remote = require('electron').remote;
 var macroQueue;
 var mainWindow = remote.getCurrentWindow();
 var contents = mainWindow.webContents;
-const { dialog } = require('electron').remote
+const { dialog, app } = require('electron').remote
 function close() {
   mainWindow.close();
 }
@@ -17,21 +17,24 @@ function electron(user) {
   if (window && window.process && window.process.type) {
     console.log("electron!!")
     console.log(contents)
+    app.setLoginItemSettings({
+      openAtLogin: false,
+    })
     macroQueue = firebase.database().ref("macroqueue/" + cuser.uid);
     macroQueue.on("child_added", function (snapshot) {
       // console.log(snapshot.val());
       cmd.get(snapshot.val(), console.log)
       snapshot.ref.remove();
     });
-    
+
     input = document.querySelector("#close-button");
-   
+
     input.addEventListener("click", function (event) {
       hideWindow();
     });
     electronUI(true);
     isElectron = true;
-  }else{
+  } else {
     electronUI(false);
     isElectron = false;
   }
